ecd685aa88f46e3cfff0cce8252d8d2e6902e9e8
Maor RG <maor.roizman@utec.edu.pe>
2020-09-16 11:03:47 -0500

    First version

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -6,11 +6,37 @@ import java.util.logging.Logger;
 public class  DNASequencer {
 
     static final Logger logger = Logger.getLogger(DNASequencer.class.getName());
-    public DNASequencer() {
-        logger.info("Starting sequencer...");
-    }
+    
+    public String DNASequencer(List<String> subsequences) {
+        StringBuilder LCB = new StringBuilder();
+        LCB.append(subsequences.get(0));
+        int totalOfSubsequences = subsequences.size();
+        int currentNumberOfSubsequence = 1;
+
+        if(totalOfSubsequences == 1){
+            return LCB.toString();
+        }
+
+        while(currentNumberOfSubsequence < totalOfSubsequences){
+            String currentSubsequence = subsequences.get(currentNumberOfSubsequence);
+            StringBuilder subsequenceChars = new StringBuilder();
+            int currentSubsequenceSize = currentSubsequence.length();
+            for(int j=0; j < currentSubsequenceSize; j++){
+                char e = currentSubsequence.charAt(j);
+                String eToString = Character.toString(e);
+                subsequenceChars.append(eToString);
+
+                if(!(LCB.toString()).contains(subsequenceChars)){
+                    LCB.append(eToString);
+                }
+            }
+            currentNumberOfSubsequence++;
+        }
+        return LCB.toString();
+	}
 
     public String calculate(List<String> part){
-        return "AGATTACAGA";
+        String LCB = DNASequencer(part);
+        return LCB;
     }
 }
